e6dea7e3e5398aff9673d4fd32c6e90a0bec9719
Le Phuoc Huy <huynoob1201@gmail.com>
2025-07-13 18:29:23 +0700

    Push lại Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Trang đầu tiên của tôi</title>
+</head>
+<body>
+    <h1>👋 Xin chào Laravel!</h1>
+    <p>Đây là website đầu tiên của mình làm bằng Laravel.</p>
+</body>
+</html>
